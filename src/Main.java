feb8f85e581c577d9d56462a229a38ad923f1b16
cmecu <howardtaylor.dev@gmail.com>
2025-02-14 19:57:30 -0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,10 @@
 import dbase.DbaseConnection;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+       DbaseConnection.loginMenu();
+       DbaseConnection.mainMenu();
 
-        System.out.print("Student ID: ");
-        int studentID = sc.nextInt();
-        System.out.println(DbaseConnection.checkStudent(studentID));
     }
 }
 
--- src/dbase/DbaseConnection.java
+++ src/dbase/DbaseConnection.java
@@ -1,10 +1,46 @@
 package dbase;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class DbaseConnection {
+    public static int studentID;
+    public static String password;
+    public static String first_name;
+
+    // Display Login Menu on screen
+    public static void loginMenu(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("<< Student Information System >>\n" +
+                "Login to Continue");
+
+        while(true) {
+            System.out.print("Student ID: ");
+            // Check for valid studentID
+            if(sc.hasNextInt()){
+                studentID = sc.nextInt();
+                studentID = studentID == 0 ? -1 : studentID;
+            }else{
+                sc.next();
+            }
+
+            if(DbaseConnection.checkStudent(studentID)){
+                break;
+            }
+            System.out.println("Invalid ID");
+        }
+
+        while(true) {
+            System.out.print("Password: ");
+            password = sc.next();
+            if(DbaseConnection.checkPassword(studentID, password)){
+                break;
+            }
+            System.out.println("Invalid Password");
+        }
+    }
     //check if studentID exists
-    public static String checkStudent(int studentID) {
+    public static boolean checkStudent(int studentID) {
         try {
             Connection con = DriverManager.getConnection(
                     LoginConstants.DB_URL,
@@ -18,18 +54,54 @@ public class DbaseConnection {
             );
 
             checkStudentIdExists.setInt(1, studentID);
-
             ResultSet resultSet = checkStudentIdExists.executeQuery();
 
             if(!resultSet.isBeforeFirst()){
-                return "False: Invalid Student ID";
-
+                return false;
             }
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+        return true;
+    }
 
-        return "true";
+    //check if studentID exists
+    public static boolean checkPassword(int studentID, String password) {
+        try {
+            Connection con = DriverManager.getConnection(
+                    LoginConstants.DB_URL,
+                    LoginConstants.DB_USERNAME,
+                    LoginConstants.DB_PASSWORD
+            );
+
+            PreparedStatement validateStudent = con.prepareStatement(
+                    "SELECT * FROM " + LoginConstants.DB_TABLE_NAME +
+                            " WHERE STUDENT_ID = ? AND PASSWORD = ?"
+            );
+
+            validateStudent.setInt(1, studentID);
+            validateStudent.setString(2, password);
+            ResultSet resultSet = validateStudent.executeQuery();
+
+            if(!resultSet.isBeforeFirst()){
+                return false;
+            }
+
+            while(resultSet.next()){
+                first_name = resultSet.getString(2);
+                System.out.println("Hello " + first_name +
+                        "! What would you like to do?");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return true;
+    }
+
+    public static void mainMenu() {
+        System.out.println("Main Menu");
+
     }
 }
